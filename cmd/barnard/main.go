28c2f499bf9b57868e20ccfb157b43ed87d5dfb7
Erol Guzoğlu <erolguzoglu@gmail.com>
2016-04-30 09:14:13 +0300

    Adapted main.go to restful api

--- cmd/barnard/main.go
+++ cmd/barnard/main.go
@@ -18,7 +18,8 @@ func main() {
 	server := flag.String("server", "localhost:64738", "the server to connect to")
 	username := flag.String("username", "lolli", "the username of the client")
 	insecure := flag.Bool("insecure", true, "skip server certificate verification")
-	certificate := flag.String("certificate", "", "PEM encoded certificate and private key")
+	certificate := flag.String("certificate", "server.pem", "PEM encoded certificate and private key")
+	key := flag.String("key", "server.key", "PEM encoded certificate and private key")
 
 	flag.Parse()
 
@@ -34,14 +35,15 @@ func main() {
 		b.TLSConfig.InsecureSkipVerify = true
 	}
 	if *certificate != "" {
-		cert, err := tls.LoadX509KeyPair(*certificate, *certificate)
+		cert, err := tls.LoadX509KeyPair(*certificate, *key)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err)
 			os.Exit(1)
 		}
 		b.TLSConfig.Certificates = append(b.TLSConfig.Certificates, cert)
 	}
-	mux := initializeApiMethods()
+
+	mux := b.InitializeApiMethods()
 	b.Api = negroni.Classic()
 	b.Api.UseHandler(mux)
 	go b.Api.Run(":3000")
